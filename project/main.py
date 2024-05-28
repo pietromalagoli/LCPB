e06c6bc7872374740024b67e36cf35d0d0d39e48
FedericoBraidi <federico@braidi.com>
2024-05-28 17:59:53 +0200

    graphs are better now

--- project/main.py
+++ project/main.py
@@ -15,10 +15,10 @@ from tqdm import tqdm
 
 ##########IMPORT PARAMETERS##########
 cwd = os.getcwd()
-dir_names = ['MESA-Web_M07_Z00001', 'MESA-Web_M07_Z002']  # type 'all' if you want to use all the data
+dir_names = ['MESA-Web_M07_Z00001', 'MESA-Web_M10_Z002']  # type 'all' if you want to use all the data
 column_filter = ['mass', 'radius', 'initial_mass', 'initial_z', 'star_age', 'logRho', 'logT',
                  'Teff', 'energy', 'photosphere_L', 'photosphere_r', 'star_mass', 'h1', 'he3', 'he4']
-column_filter_train = ['logRho']  # radius is not included for coding reasons but is still considered
+column_filter_train = ['logRho','mass']  # radius is not included for coding reasons but is still considered
 n_points = 100  # n of points to sample from each profile
 r = np.linspace(0, 1, n_points + 1)[1:]  # values of normalized r on which to take the values of the variables
 
@@ -80,15 +80,16 @@ for i, dir_name in enumerate(tqdm(dir_names, desc="Importing data from directori
         norm_profiles = []
 
         for k, column in enumerate(column_filter_train):
-            norm = (filtered_profile_df[column] - filtered_profile_df[column].min()) / \
-                   (filtered_profile_df[column].max() - filtered_profile_df[column].min())
+            #norm = (filtered_profile_df[column] - filtered_profile_df[column].min()) / \
+            #       (filtered_profile_df[column].max() - filtered_profile_df[column].min())
+            norm = filtered_profile_df[column]
             norm = np.asarray(norm.T)
             int_norm = UnivariateSpline(norm_radius, norm, k=2, s=0)(r)
             norm_profiles.append(int_norm)
-            print("Length of this profile: ",len(norm_profiles))
+            #print("Length of this profile: ",len(norm_profiles))
 
         all_profiles.append(np.array(norm_profiles).T)
-        print("Num profiles: ", len(all_profiles))
+        #print("Num profiles: ", len(all_profiles))
 
 # Convert the list of profiles to a numpy array
 all_profiles = np.array(all_profiles)
@@ -99,14 +100,22 @@ x_train, x_test = train_test_split(all_profiles, test_size=0.2)
 print(x_train.shape)
 print(x_test.shape)
 
+# reshape in the form n_prof*(n_points*n_features)
+train_shape=x_train.shape
+test_shape=x_test.shape
+x_train=np.reshape(x_train,(train_shape[0],train_shape[1]*train_shape[2]),order="F")
+x_test=np.reshape(x_test,(test_shape[0],test_shape[1]*test_shape[2]),order="F")
+print(x_train.shape)
+print(x_test.shape)
+
 for i in range(1, 10):
     ###Autoencoder parameters
     encoder_neurons = [100, i]  # last value should be the latent dimension
     decoder_neurons = encoder_neurons[:len(encoder_neurons) - 1][::-1]  # same inverted values for the hidden layers
     decoder_neurons.append(n_points*len(column_filter_train))  # last value should be original size
-    encoder_activations = ['relu'] * len(encoder_neurons)  # with relu better learning
-    decoder_activations = ['relu'] * (len(encoder_neurons) - 1)  # relu for better learning
-    decoder_activations.append('sigmoid')  # at least the last value should be sigmoid
+    encoder_activations = ['leaky_relu'] * len(encoder_neurons)  # with relu better learning
+    decoder_activations = ['leaky_relu'] * (len(encoder_neurons) - 1)  # relu for better learning
+    decoder_activations.append('leaky_relu')  # at least the last value should be sigmoid
 
     autoencoder = Autoencoder(encoder_neurons=encoder_neurons, decoder_neurons=decoder_neurons,
                               encoder_activations=encoder_activations, decoder_activations=decoder_activations)
@@ -133,21 +142,34 @@ for i in range(1, 10):
     print(x_reconstructed.shape)
 
     # Plot and save original vs reconstructed profiles
-    n = 10  # How many profiles we will display
-    plt.figure(figsize=(20, 4))
-    plt.axis("off")
-    plt.title(f'Examples of fit with latent dimension = {i}')
-    for j in range(n):
+    n=5
+    
+    for k,feature in enumerate(column_filter_train):
+        plt.figure(figsize=(20, 10))
+        plt.axis("off")
+        plt.title(f'Examples of fit of feature {feature} with latent dimension = {i}')
         # Display original
-        ax = plt.subplot(2, n, j + 1)
-        plt.scatter(r, x_test[j].reshape(-1, n_points)[0])
-        plt.gray()
+        for j in range(n):
+            print(x_test)
+            print(x_test.shape)
+            original_data=x_test[j,k*n_points:(k+1)*n_points]
+            print(original_data.shape)
+            reconstructed_data=x_reconstructed[j,k*n_points:(k+1)*n_points]
+            print(reconstructed_data.shape)
+            diff=original_data-reconstructed_data
 
-        # Display reconstruction
-        ax = plt.subplot(2, n, j + 1 + n)
-        plt.scatter(r, x_reconstructed[j].reshape(-1, n_points)[0])
-        plt.gray()
+            ax = plt.subplot(2, n, j + 1)
+            plt.scatter(r, original_data,c="blue",label="original")
+            plt.scatter(r, reconstructed_data,c="red",label="reconstructed")
+            plt.legend()
+            plt.gray()
 
-    file_save_dir = os.path.join(os.getcwd(), "Graphs", f"OriginalReconstructed_{i}.png")
-    plt.savefig(file_save_dir)
-    plt.close()
+            #Display reconstruction
+            ax = plt.subplot(2, n, j + 1 + n)
+            plt.scatter(r, diff,label="Original-Reconstructed",c="green")
+            plt.legend()
+            plt.gray()
+
+        file_save_dir = os.path.join(os.getcwd(), "Graphs", f"OriginalReconstructed_{i}_{feature}.png")
+        plt.savefig(file_save_dir)
+        plt.close()
